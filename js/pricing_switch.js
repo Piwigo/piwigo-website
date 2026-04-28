939db61b42e6a17d5484f9230f2ed0bad7f02cfd
Zahra Salou <zahrasalou28@gmail.com>
2026-04-28 15:14:18 +0200

    modification of the pricing_switch.js for the url storage

--- js/pricing_switch.js
+++ js/pricing_switch.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-    function updatePricingCards(type) {
+    // to handle the plan parameter in the URL
+    let searchParams = new URLSearchParams(window.location.search);
+
+    /******
+    Updates the UI based on selected plan
+    ******/
+    function updatePricingCards(type, updateUrl = true) {
         if (type === 'cloud') {
             $('#pricing-selfhosted-content').hide();
             $('#pricing-cloud-content').fadeIn(300);
@@ -13,24 +19,35 @@ $(document).ready(function() {
             $('#desc-selfhosted').removeClass('d-none').fadeIn(300);
             $('#card-selfhosted').removeClass('d-none-card');
         }
+        if (updateUrl) {
+            const urlValue = (type === 'cloud') ? 'cloud' : 'self_hosted';
+            searchParams.set('plan', urlValue);
+            const newUrl = window.location.pathname + '?' + searchParams.toString();
+            window.history.pushState(null, '', newUrl);
+
+            localStorage.setItem('selectedPricing', type);
+        }
     }
 
+    // Toggle switch 
     $('.btn-pricing').on('click', function() {
         var $clickedBtn = $(this);
         if ($clickedBtn.hasClass('active')) return;
-
         var $parent = $clickedBtn.closest('.btn-pricing-switch');
 
         $parent.find('.btn-pricing').toggleClass('active inactive bg-white bg-transparent shadow-sm shadow-none');
-
         var choice = $clickedBtn.hasClass('cloud') ? 'cloud' : 'self-hosted';
         updatePricingCards(choice);
-        
-        // localStorage.setItem('selectedPricing', choice);
     });
 
-    // var saved = localStorage.getItem('selectedPricing');
-    // if (saved && saved === 'self-hosted') {
-    //     $('.btn-pricing.self-hosted').trigger('click');
-    // }
+    // Initialization 
+    const planParam = searchParams.get('plan');
+    const savedChoice = localStorage.getItem('selectedPricing');
+    if (planParam === 'self_hosted' || (!planParam && savedChoice === 'self-hosted')) {
+        $('.btn-pricing.cloud').removeClass('active bg-white shadow-sm').addClass('inactive bg-transparent shadow-none');
+        $('.btn-pricing.self-hosted').addClass('active bg-white shadow-sm').removeClass('inactive bg-transparent shadow-none');
+        updatePricingCards('self-hosted', false);
+    } else {
+        updatePricingCards('cloud', false);
+    }
 });
