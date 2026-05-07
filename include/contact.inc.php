d8441bfe3d82d69401aa40486a08ad53c2bd73c6
Martin COZIC <martin.cozic@etu.univ-nantes.fr>
2026-05-07 16:34:00 +0200

    no category anymore

--- include/contact.inc.php
+++ include/contact.inc.php
@@ -3,14 +3,4 @@
     global $template;
 
     $template->assign('EPHEMERAL_KEY', get_ephemeral_key(2));
-
-    check_input_parameter('type', $_GET, false, '/^(testimonial|security|press|partnership|beta testing)$/');
-    if (isset($_GET['type'])) 
-    {
-        $category = $_GET['type'];
-    }
-
-    $template->assign(array(
-        'category' => isset($category) ? $category : null,
-    ));
 ?>
