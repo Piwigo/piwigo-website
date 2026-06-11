7dc831964de442b089bb016cc8ef8412ad52854f
MartinCozic <martincozic500@gmail.com>
2026-06-11 12:32:29 +0200

    fix indentation

--- include/signup.inc.php
+++ include/signup.inc.php
@@ -46,26 +46,26 @@ $nb_accounts = 10;
 $median_duration = 9.6;
 $max_account_id = null;
 
-$signup_host_badge ='europe-host.svg';
+$signup_host_badge = 'europe-host.svg';
 
 if (isset($user['language']) && $user['language'] === 'fr_FR') {
-	$signup_host_badge ='france-host.svg';
+  $signup_host_badge = 'france-host.svg';
 }
 
 
 try {
-	$query = 'SELECT MAX(account_id) FROM ' . $db_prefix . 'accounts;';
-	$result = pwg_query($query);
-	if ($result) {
-		list($max_account_id) = pwg_db_fetch_row($result);
-	}
+  $query = 'SELECT MAX(account_id) FROM ' . $db_prefix . 'accounts;';
+  $result = pwg_query($query);
+  if ($result) {
+    list($max_account_id) = pwg_db_fetch_row($result);
+  }
 } catch (Exception $e) {
 }
 
 if (!empty($max_account_id)) {
-	$sample_size = 100;
+  $sample_size = 100;
 
-	$query = '
+  $query = '
 SELECT
     TIMESTAMPDIFF(SECOND,registered_on,installed_on) AS duration
   FROM ' . $db_prefix . 'accounts
@@ -74,33 +74,33 @@ SELECT
   ORDER BY duration ASC
 ;';
 
-	$durations = query2array($query, null, 'duration');
+  $durations = query2array($query, null, 'duration');
 
-	if (count($durations) >= 1) {
-		$nb_accounts = count($durations);
-		$median_duration = $durations[floor($nb_accounts / 2)];
-	}
+  if (count($durations) >= 1) {
+    $nb_accounts = count($durations);
+    $median_duration = $durations[floor($nb_accounts / 2)];
+  }
 }
 
 $porg_urls = porg_get_page_urls();
 
 $template->assign(
-	array(
-		'SIGNUP_HOST_BADGE' => $signup_host_badge,
-		'STATS_NB_LAST_ACOUNTS' => $nb_accounts,
-		'STATS_AVG_DURATION' => sprintf('%.1f', $median_duration),
-		'registration_enabled' => conf_get_param('registration_enabled', true),
-		'SIGNUP_CHECK_LABEL' => sprintf(
-			l10n('signup_check_RPG'),
-			$porg_urls['terms_of_service'],
-			$porg_urls['privacy']
-		),
-		'STATS_GLOBAL_INSTALLS' => l10n('%s+', '100.000'),
+  array(
+    'SIGNUP_HOST_BADGE' => $signup_host_badge,
+    'STATS_NB_LAST_ACOUNTS' => $nb_accounts,
+    'STATS_AVG_DURATION' => sprintf('%.1f', $median_duration),
+    'registration_enabled' => conf_get_param('registration_enabled', true),
+    'SIGNUP_CHECK_LABEL' => sprintf(
+      l10n('signup_check_RPG'),
+      $porg_urls['terms_of_service'],
+      $porg_urls['privacy']
+    ),
+    'STATS_GLOBAL_INSTALLS' => l10n('%s+', '100.000'),
     'STATS_GLOBAL_FILES'    => l10n('%s+ millions', '67'),
     'STATS_GLOBAL_UPTIME'   => l10n('%s uptime', '99.98%'),
     'STATS_GLOBAL_LANGS'    => l10n('+%d', 59),
     'STATS_GLOBAL_CLIENTS'  => l10n('%s+', '5.800'),
-	)
+  )
 
-	
+
 );
