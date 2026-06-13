f9190c044093f221e8d71b3475fbd4389a416ca6
plegall <plg@piwigo.org>
2026-06-13 14:01:07 +0200

    clean signup backend code

--- include/signup.inc.php
+++ include/signup.inc.php
@@ -1,92 +1,48 @@
 <?php
-/*$query = '
-SELECT
-		MAX(account_id)
-	FROM ' . PCOM_ACCOUNTS_DB_PREFIX . 'accounts
-;';
-list($max_account_id) = pwg_db_fetch_row(pwg_query($query));
+global $pwg_loaded_plugins;
 
 $nb_accounts = 10;
 $median_duration = 9.6;
 
-if (!empty($max_account_id)) {
-		$sample_size = 100;
-
-		$query = '
-SELECT
-		TIMESTAMPDIFF(SECOND,registered_on,installed_on) AS duration
-	FROM ' . PCOM_ACCOUNTS_DB_PREFIX . 'accounts
-	WHERE account_id > ' . ($max_account_id - $sample_size) . '
-		AND installed_on IS NOT NULL
-	ORDER BY duration ASC
-;';
-		$durations = query2array($query, null, 'duration');
-		if (count($durations) == $sample_size) {
-				$nb_accounts = $sample_size;
-				$median_duration = $durations[floor($sample_size / 2)];
-		}
-}
-
-$template->assign(
-		array(
-				'STATS_NB_LAST_ACOUNTS' => $nb_accounts,
-				'STATS_AVG_DURATION' => sprintf('%.1f', $median_duration),
-				'registration_enabled' => conf_get_param('registration_enabled', true),
-		)
-);
-
-if (isset($_GET['username'])) {
-		$template->assign('username', $_GET['username']);
-} */
-
-
-$db_prefix = defined('PCOM_ACCOUNTS_DB_PREFIX') ? PCOM_ACCOUNTS_DB_PREFIX : 'pcom_';
-
-$nb_accounts = 10;
-$median_duration = 9.6;
-$max_account_id = null;
-
-$signup_host_badge = 'europe-host.svg';
-
-if (isset($user['language']) && $user['language'] === 'fr_FR') {
-  $signup_host_badge = 'france-host.svg';
-}
-
-
-try {
-  $query = 'SELECT MAX(account_id) FROM ' . $db_prefix . 'accounts;';
-  $result = pwg_query($query);
-  if ($result) {
-    list($max_account_id) = pwg_db_fetch_row($result);
-  }
-} catch (Exception $e) {
-}
-
-if (!empty($max_account_id)) {
-  $sample_size = 100;
+if (isset($pwg_loaded_plugins['piwigo-website-pcomws']) and !conf_get_param('pcom_signup_simulate', true))
+{
+  accounts_db_connect();
 
   $query = '
 SELECT
+    MAX(account_id)
+  FROM accounts
+;';
+  list($max_account_id) = pwg_db_fetch_row(pwg_query($query));
+
+  if (!empty($max_account_id))
+  {
+    $sample_size = 100;
+
+    $query = '
+SELECT
     TIMESTAMPDIFF(SECOND,registered_on,installed_on) AS duration
-  FROM ' . $db_prefix . 'accounts
-  WHERE account_id > ' . ($max_account_id - $sample_size) . '
+  FROM accounts
+  WHERE account_id > '.($max_account_id - $sample_size).'
     AND installed_on IS NOT NULL
   ORDER BY duration ASC
 ;';
-
-  $durations = query2array($query, null, 'duration');
-
-  if (count($durations) >= 1) {
-    $nb_accounts = count($durations);
-    $median_duration = $durations[floor($nb_accounts / 2)];
+    $durations = query2array($query, null, 'duration');
+    if (count($durations) == $sample_size)
+    {
+      $nb_accounts = $sample_size;
+      $median_duration = $durations[floor($sample_size / 2)];
+    }
   }
+
+  accounts_db_disconnect();
 }
 
 $porg_urls = porg_get_page_urls();
 
 $template->assign(
   array(
-    'SIGNUP_HOST_BADGE' => $signup_host_badge,
+    'SIGNUP_HOST_BADGE' => 'fr_FR' == $user['language'] ? 'france-host.svg' : 'europe-host.svg',
     'STATS_NB_LAST_ACOUNTS' => $nb_accounts,
     'STATS_AVG_DURATION' => sprintf('%.1f', $median_duration),
     'registration_enabled' => conf_get_param('registration_enabled', true),
@@ -101,6 +57,9 @@ $template->assign(
     'STATS_GLOBAL_LANGS'    => l10n('+%d', 59),
     'STATS_GLOBAL_CLIENTS'  => l10n('%s+', '5.800'),
   )
-
-
 );
+
+if (isset($_GET['username']))
+{
+  $template->assign('username', $_GET['username']);
+}
