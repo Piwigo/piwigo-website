4135d612ea49d8f188e8b66159fdf2e06fa30ae1
plegall <plg@piwigo.org>
2026-08-18 17:48:26 +0200

    changes on Cotentin by Claire (import from piwigo.com)

--- language/fr_FR/case_study_cotentin.lang.php
+++ language/fr_FR/case_study_cotentin.lang.php
@@ -1,22 +1,22 @@
 <?php
-$lang['page_meta_title'] = 'Étude de cas Piwigo : une photothèque pour un office de tourisme français';
-$lang['page_meta_description'] = 'Office de Tourisme du Cotentin : une photothèque collaborative et sécurisée pour la communication touristique de 130 communes.';
+$lang['page_meta_title'] = 'Cas client Piwigo - Office de Tourisme du Cotentin';
+$lang['page_meta_description'] = 'Office de Tourisme du Cotentin : une photothèque collaborative pour la communication touristique de 130 communes';
 
 $lang['porg_cases_cotentin_title'] = 'L\'Office de Tourisme du Cotentin simplifie le partage et la diffusion de ses médias numériques';
-$lang['porg_cases_cotentin_description'] = 'La « Société Publique Locale de développement touristique du Cotentin » a été créée en 2017 ; elle est née de la fusion des offices de tourisme du Cotentin, en Normandie...<br><br>Elle gère la communication touristique du territoire et son site web <a class="orange-text" href="http://www.encotentin.fr " target="_blank">http://www.encotentin.fr</a>.';
+$lang['porg_cases_cotentin_description'] = 'La Société Publique Locale de développement touristique du Cotentin est née en 2017 du regroupement des Offices de tourisme du Cotentin. Elle gère la communication touristique de plus de 130 communes à travers ses 21 bureaux d’information et son site Internet <a class="orange-text" href="https://www.encotentin.fr" target="_blank">encotentin.fr</a>.';
 $lang['porg_cases_start_title'] = 'Prêt à gérer vos photos avec Piwigo ?';
 $lang['porg_cases_start_description'] = 'Essayez Piwigo Cloud gratuitement pendant 30 jours. Créez votre compte, importez vos fichiers et commencez à travailler immédiatement.';
 
-$lang['porg_cases_cotentin_challenge_title'] = 'Le défi : simplifier l\'organisation et le partage des données';
-$lang['porg_cases_cotentin_challenge_description'] = 'Christine Semilly-Lagalle, assistante en charge du service Communication, s\'occupe de la photothèque de l\'Office de Tourisme du Cotentin. Les photos servent à réaliser les brochures, à alimenter le site web et les réseaux sociaux. La place de l\'image est très importante dans la communication touristique : elle joue donc un rôle clé.<br><br>Pourtant, l\'organisation en place avant Piwigo n\'était pas satisfaisante : la photothèque était stockée sur un disque dur externe partagé entre les membres de l\'équipe. Après les prises de vue, les photographes distribuaient les photos sur CD. Le partage vers l\'extérieur se faisait via WeTransfer. Accéder simplement à un contenu était compliqué, tout prenait du temps. Le risque de perte de données était important. Il a donc été décidé de mettre en place une photothèque partagée.';
+$lang['porg_cases_cotentin_challenge_title'] = 'Le besoin : simplifier l\'organisation et le partage des données';
+$lang['porg_cases_cotentin_challenge_description'] = 'Christine Semilly-Lagalle, assistante responsable du service Communication, est en charge de la photothèque de l\'Office de Tourisme du Cotentin. Les photos servent à créer les brochures et à alimenter le site Internet et les réseaux sociaux. La place de l\'image est très importante dans la communication touristique : il s\'agit donc d\'un rôle clé.<br><br>Pourtant, l\'organisation en place avant Piwigo n\'était pas satisfaisante : la photothèque était stockée sur un disque dur externe partagé entre les membres de l\'équipe. Après les prises de vue, les photographes distribuaient les photos sur CD. Le partage externe se faisait via WeTransfer. Il était difficile d\'accéder simplement aux contenus et chaque opération prenait du temps. Le risque de perte de données était important. Il a donc été décidé de mettre en place une photothèque partagée.';
 
 $lang['The choice of Piwigo'] = 'Le choix de Piwigo';
-$lang['porg_cases_cotentin_choice_description'] = 'Après des recherches et des comparaisons d\'outils, Piwigo a été retenu pour mettre en place la photothèque en ligne de l\'Office de Tourisme. Il fallait une solution simple à utiliser, capable de gérer différents droits d\'accès (administration, ajout de photos, recherche de photos...)... et à un coût raisonnable.';
+$lang['porg_cases_cotentin_choice_description'] = 'Après avoir recherché et comparé plusieurs outils, l\'Office de Tourisme a retenu Piwigo pour mettre en place sa photothèque en ligne. Il lui fallait une solution simple d\'utilisation, permettant de gérer différents droits d\'accès (administration, ajout et recherche de photos, etc.), le tout à un coût raisonnable.';
 
-$lang['Benefits'] = 'Les bénéfices';
-$lang['porg_cases_cotentin_benefits_description'] = 'Aujourd\'hui, Piwigo est utilisé par une vingtaine de personnes : 4 administrateurs, une dizaine d\'utilisateurs internes qui viennent « faire leur marché » dans la photothèque, mais aussi des utilisateurs externes (photographes, agence de communication). Le partage de fichiers, en interne mais aussi avec les partenaires et les autres administrations, est devenu beaucoup plus rapide et plus simple ! Toute l\'organisation est fluidifiée.';
-$lang['porg_cases_cotentin_benefits_text'] = 'Piwigo nous permet de conserver nos photos dans un espace sécurisé et partagé : c\'est un outil collaboratif qui facilite grandement le partage de fichiers, en interne mais aussi avec nos prestataires et nos partenaires. Je suis satisfaite de l\'utilisation de Piwigo.';
-$lang['porg_cases_cotentin_benefits_person'] = 'Assistante en charge du service Communication';
+$lang['Benefits'] = 'Des résultats au rendez-vous';
+$lang['porg_cases_cotentin_benefits_description'] = 'Aujourd\'hui, Piwigo est utilisé par une vingtaine de personnes : 4 administrateurs, une dizaine d\'utilisateurs internes qui accèdent à la photothèque pour « faire leur marché », mais aussi des utilisateurs externes, notamment des prestataires. Le partage de fichiers, en interne comme avec les partenaires et les autres administrations, est devenu beaucoup plus rapide et plus simple. Toute l\'organisation s\'en trouve fluidifiée.';
+$lang['porg_cases_cotentin_benefits_text'] = 'Piwigo nous permet de sauvegarder nos photos dans un espace sécurisé et partagé : c\'est un outil collaboratif qui facilite grandement le partage de fichiers, en interne comme avec nos prestataires et partenaires. Je suis satisfaite de l\'utilisation de Piwigo.';
+$lang['porg_cases_cotentin_benefits_person'] = 'Assistante responsable du service Communication';
 $lang['porg_cases_cotentin_benefits_structure'] = 'Office de Tourisme du Cotentin';
 
 $lang['More case studies'] = 'Plus d\'études de cas';
