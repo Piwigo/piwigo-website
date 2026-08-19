606f5e2d5b679a967e25e22f7eed3b2c2dc84c01
Martin COZIC <martin.cozic@etu.univ-nantes.fr>
2026-08-19 12:09:38 +0200

    change back base language to english

--- main.inc.php
+++ main.inc.php
@@ -37,7 +37,7 @@ function porg_user_init()
   global $user, $page;
 
   $page['porg_domain_prefix'] = '';
-  $user['language'] = 'fr_FR';
+  $user['language'] = 'en_UK';
 
   $raw_url = $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 
