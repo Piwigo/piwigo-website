e50517539c205936f9f9fa0e0e22bca41695ac72
clairem44 <127497258+clairem44@users.noreply.github.com>
2026-08-20 15:50:51 +0200

    Update feature_share.lang.php - corrections Claire

--- language/fr_FR/feature_share.lang.php
+++ language/fr_FR/feature_share.lang.php
@@ -1,23 +1,23 @@
 <?php
 // share
 
-$lang['share title'] = 'Collaborez sur vos médias avec vos équipes et vos partenaires';
-$lang['share description'] = 'Donnez à chaque utilisateur le bon niveau d\'accès et collaborez facilement avec vos équipes internes ou vos contributeurs externes.';
-$lang['share text 1'] = 'Partagez facilement vos médias avec vos équipes et vos partenaires';
-$lang['share text 2'] = 'Contrôlez précisément qui peut consulter, ajouter et modifier les médias';
+$lang['share title'] = 'Collaborez facilement avec vos équipes et vos partenaires';
+$lang['share description'] = 'Donnez à chaque utilisateur le niveau d\'accès adapté et collaborez facilement avec vos équipes internes ou vos contributeurs externes.';
+$lang['share text 1'] = 'Partagez facilement vos médias avec vos équipes et partenaires';
+$lang['share text 2'] = 'Définissez précisément qui peut consulter, ajouter ou modifier les médias';
 $lang['share text 3'] = 'Travaillez ensemble autour d\'une médiathèque unique et partagée';
 
 $lang['share sub1 title'] = 'Utilisateurs et groupes';
-$lang['share sub1 description'] = 'Avec Piwigo, invitez un nombre illimité d\'utilisateurs dans votre photothèque et adaptez leurs permissions à leurs besoins. Administrateurs, contributeurs ou simples visiteurs : vous pouvez gérer différents niveaux d\'accès et personnaliser finement les permissions - envoi et téléchargement de fichiers, accès aux albums… Les permissions se définissent par utilisateur ou par groupe, selon votre organisation.';
+$lang['share sub1 description'] = 'Avec Piwigo, invitez un nombre illimité d\'utilisateurs dans votre photothèque et adaptez leurs permissions à leurs besoins. Administrateurs, contributeurs ou simples visiteurs : vous pouvez gérer différents niveaux d\'accès et personnaliser finement les permissions : ajout et téléchargement de fichiers, accès aux albums… Les permissions se définissent par utilisateur ou par groupe, en fonction de votre organisation.';
 
 $lang['share sub2 title'] = 'Partage par lien';
-$lang['share sub2 description'] = "Besoin de partager un fichier ou un album avec une personne extérieure à votre organisation ? Vous pouvez lui envoyer un lien de téléchargement par email, même si elle n'a pas de compte utilisateur sur votre photothèque. Vous partagez ainsi des contenus vers l'extérieur sans ouvrir l'accès à toute votre médiathèque.";
+$lang['share sub2 description'] = "Besoin de partager un fichier ou un album avec une personne extérieure à votre organisation ? Vous pouvez lui envoyer un lien de téléchargement par e-mail, même si elle n'a pas de compte utilisateur sur votre photothèque. Vous partagez ainsi des contenus en externe sans ouvrir l'accès à toute votre médiathèque.";
 
 $lang['share sub3 title'] = 'Galerie publique ou privée';
-$lang['share sub3 description'] = "Piwigo vous permet de créer de belles galeries photo publiques, pour les photographes qui veulent exposer leur travail ou pour les organisations qui partagent leur photothèque sur internet. Mais votre photothèque peut aussi être entièrement privée, et accessible aux seuls utilisateurs identifiés. Vous pouvez également mélanger les deux approches, avec une partie privée et une partie publique : c'est vous qui voyez ! Vous décidez de ce qui est public et de ce qui reste privé.";
+$lang['share sub3 description'] = "Piwigo vous permet de créer de belles galeries photo publiques, pour les photographes qui souhaitent exposer leur travail ou pour les organisations qui partagent leur photothèque avec le public. Mais votre photothèque peut aussi être entièrement privée, et accessible aux seuls utilisateurs identifiés. Vous pouvez également mélanger les deux approches, avec une partie privée et une partie publique : à vous de choisir ! Vous décidez de ce qui est public et de ce qui reste privé.";
 
 $lang['share sub4 title'] = 'Options de téléchargement';
 $lang['share sub4 description'] = "Choisissez qui peut télécharger les fichiers de votre médiathèque. Pour chaque utilisateur, vous décidez s'il peut uniquement consulter les fichiers (en basse résolution) ou les télécharger. Vous pouvez aussi désactiver complètement les téléchargements.";
 
 $lang['share sub5 title'] = 'Collaboration et commentaires';
-$lang['share sub5 description'] = "Avec Piwigo, les équipes peuvent collaborer et échanger en ligne de différentes manières : en notant les fichiers, en discutant via les commentaires, en partageant des albums par email…";
+$lang['share sub5 description'] = "Avec Piwigo, les équipes peuvent collaborer et échanger de différentes façon : en notant les fichiers, en discutant dans les commentaires, en partageant des albums par email…";
