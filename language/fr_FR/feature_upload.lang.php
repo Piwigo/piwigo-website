441df38358e67f2c3715fdbcd714e53b2f912523
clairem44 <127497258+clairem44@users.noreply.github.com>
2026-08-20 15:18:41 +0200

    Update feature_upload.lang.php - Corrections Claire

--- language/fr_FR/feature_upload.lang.php
+++ language/fr_FR/feature_upload.lang.php
@@ -1,21 +1,21 @@
 <?php
 // upload
 
-$lang['upload title'] = 'Rassemblez tous vos médias dans une médiathèque centralisée';
-$lang['upload description'] = 'Importez vos médias depuis plusieurs sources, profitez de la prise en charge de tous les formats de fichiers et laissez votre équipe contribuer à une médiathèque unique et bien organisée.';
-$lang['upload text 1'] = 'Fini les allers-retours entre disques durs et dossiers éparpillés';
-$lang['upload text 2'] = 'Gardez tous vos médias dans une médiathèque centralisée';
-$lang['upload text 3'] = 'Évitez les doublons et les fichiers perdus';
+$lang['upload title'] = 'Rassemblez tous vos médias dans une photothèque centralisée';
+$lang['upload description'] = 'Importez vos fichiers depuis diverses sources, quel que soit leur format, et permettez à toute votre équipe de contribuer.';
+$lang['upload text 1'] = 'Fini les fichiers dispersés entre disques durs et dossiers partagés';
+$lang['upload text 2'] = 'Centralisez tous vos médias dans une photothèque unique';
+$lang['upload text 3'] = 'Plus de doublons ni de perte de fichier';
 
-$lang['upload sub1 title'] = 'Import de fichiers depuis plusieurs sources';
-$lang['upload sub1 description'] = 'Votre panneau d\'administration en ligne vous permet d\'envoyer facilement des fichiers dans Piwigo, en un seul clic.<br>Vous pouvez aussi synchroniser un répertoire de votre disque dur, ou importer des fichiers depuis l\'une des applications compatibles - digiKam, Lightroom, Shotwell...';
+$lang['upload sub1 title'] = 'Import de fichiers depuis multi-sources';
+$lang['upload sub1 description'] = 'Votre espace d\'administration web vous permet d\'ajouter facilement des fichiers à Piwigo, en un seul clic.<br>Vous pouvez aussi synchroniser un répertoire de votre disque dur ou importer des fichiers depuis l\'une des applications compatibles : digiKam, Lightroom, Shotwell…';
 
 $lang['upload sub2 title'] = 'Ajoutez des photos depuis votre téléphone';
 $lang['upload sub2 description'] = 'Téléchargez l\'application mobile Piwigo pour Android ou iOS et envoyez des photos dans votre photothèque directement depuis votre smartphone : très pratique pour les équipes sur le terrain !';
 $lang['upload sub2 btn'] = 'En savoir plus sur les applications mobiles';
 
-$lang['upload sub3 title'] = 'Laissez vos collègues et vos partenaires envoyer des fichiers';
-$lang['upload sub3 description'] = 'Avec Piwigo, vous pouvez autoriser des tiers à envoyer des fichiers dans votre photothèque, sans leur donner de droits d\'administration. Vos équipes et vos contributeurs externes gagnent du temps (finis les emails et les transferts de gros fichiers !)';
+$lang['upload sub3 title'] = 'Autorisez vos collègues et partenaires à ajouter des fichiers';
+$lang['upload sub3 description'] = 'Avec Piwigo, vous pouvez autoriser des utilisateus à ajouter des fichiers à votre photothèque sans leur accorder de droits d\'administration. Vos équipes et vos contributeurs externes gagnent ainsi du temps : finis les e-mails et les transferts de fichiers volumineux !';
 
-$lang['upload sub4 title'] = 'De nombreux formats de fichiers pris en charge';
+$lang['upload sub4 title'] = 'De nombreux formats de fichiers compatibles';
 $lang['upload sub4 description'] = 'Piwigo prend en charge de nombreux formats d\'images (JPG, PNG, TIFF, RAW, etc.) et de vidéos (MP4, MOV, OGG). Vous pouvez aussi importer vos documents PDF (brochures, présentations, créations graphiques...), ainsi que vos fichiers sources (PSD, AI, etc.), et tout autre type de fichier.';
