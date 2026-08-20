de8bb816ef86ec04de3f450203109cb77f7bf97d
clairem44 <127497258+clairem44@users.noreply.github.com>
2026-08-20 16:13:47 +0200

    Update feature_measure.lang.php - corrections claire

--- language/fr_FR/feature_measure.lang.php
+++ language/fr_FR/feature_measure.lang.php
@@ -1,17 +1,17 @@
 <?php
 // measure
 
-$lang['measure title'] = 'Comprenez comment votre médiathèque est utilisée';
-$lang['measure description'] = 'Gardez de la visibilité sur la façon dont vos médias sont consultés et utilisés, pour prendre de meilleures décisions et rester maître de votre médiathèque.';
-$lang['measure text 1'] = 'Suivez l\'usage et l\'activité de votre photothèque';
-$lang['measure text 2'] = 'Comprenez comment vos utilisateurs interagissent avec vos albums, vos tags et vos médias';
-$lang['measure text 3'] = 'Connectez vos propres outils d\'analyse si besoin';
+$lang['measure title'] = 'Mesurez et pilotez l\'usage de votre photothèque';
+$lang['measure description'] = 'Obtenez une vision claire de la manière dont vos médias sont consultés et utilisés. Prenez de meilleures décisions et gardez le contrôle.';
+$lang['measure text 1'] = 'Analysez l\'activité de vos utilisateurs et leurs usages';
+$lang['measure text 2'] = 'Comprenez comment vos utilisateurs utilisent vos albums, vos tags et vos médias';
+$lang['measure text 3'] = 'Connectez vos propres outils d\'analyse si nécessaire';
 
-$lang['measure sub1 title'] = 'Analyse d\'usage intégrée';
-$lang['measure sub1 description'] = 'Avec Piwigo, vous disposez de graphiques clairs et faciles à lire pour comprendre comment votre photothèque est utilisée.<br>Consultez l\'historique des visites par jour, par mois ou par année, et suivez l\'évolution des usages dans le temps.<br>Une interface de recherche avancée vous permet d\'explorer l\'activité en détail sur une période donnée, et d\'identifier les albums, les tags et les fichiers les plus consultés.';
+$lang['measure sub1 title'] = 'Statistiques d\'usage intégrées';
+$lang['measure sub1 description'] = 'Avec Piwigo, vous disposez de graphiques clairs et faciles à lire pour comprendre comment votre photothèque est utilisée.<br>Consultez l\'historique des visites par jour, par mois ou par année, et suivez l\'évolution des usages dans le temps.<br>Un moteur de recherche avancé vous permet d\'explorer l\'activité en détail sur une période donnée, et d\'identifier les albums, les tags et les fichiers les plus consultés.';
 
 $lang['measure sub2 title'] = 'Gardez un œil sur ce qui se passe';
-$lang['measure sub2 description'] = "Avec Piwigo, toutes les actions réalisées sur votre photothèque sont suivies et enregistrées. Vous pouvez surveiller l'activité des utilisateurs en détail : envois de fichiers, modifications, suppressions et interactions. De quoi comprendre comment vos équipes ou vos utilisateurs travaillent avec vos médias et garder la maîtrise de vos contenus.";
+$lang['measure sub2 description'] = 'Avec Piwigo, toutes les actions réalisées dans votre photothèque sont suivies et enregistrées. Vous pouvez consulter en détail l\'activité des utilisateurs : ajouts de fichiers, modifications, suppressions et interactions. Vous comprenez ainsi comment vos équipes et vos utilisateurs travaillent avec vos médias, tout en gardant la maîtrise de vos contenus.';
 
 $lang['measure sub3 title'] = 'Intégration d\'outils d\'analyse personnalisés';
-$lang['measure sub3 description'] = "Si vous avez besoin d'analyses plus poussées, vous pouvez facilement intégrer vos propres outils en ajoutant des scripts de suivi dans Piwigo.<br>Vous utilisez ainsi la solution d'analyse de votre choix : Matomo, Google Analytics ou tout autre outil compatible.";
+$lang['measure sub3 description'] = 'Si vous avez besoin d\'analyses plus poussées, vous pouvez facilement intégrer vos propres outils en ajoutant des scripts de suivi dans Piwigo.<br>Vous pouvez ainsi utiliser la solution d\'analyse de votre choix : Matomo, Google Analytics ou tout autre outil compatible.';
