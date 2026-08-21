a7c55294aa05a83f6d1bf09c8af3815e89b3a33c
clairem44 <127497258+clairem44@users.noreply.github.com>
2026-08-21 10:32:38 +0200

    Update feature_upload.lang.php - correction coquille

--- language/fr_FR/feature_upload.lang.php
+++ language/fr_FR/feature_upload.lang.php
@@ -7,7 +7,7 @@ $lang['upload text 1'] = 'Fini les fichiers dispersés entre disques durs et dos
 $lang['upload text 2'] = 'Centralisez tous vos médias dans une photothèque unique';
 $lang['upload text 3'] = 'Plus de doublons ni de perte de fichier';
 
-$lang['upload sub1 title'] = 'Import de fichiers depuis multi-sources';
+$lang['upload sub1 title'] = 'Import de fichiers multi-sources';
 $lang['upload sub1 description'] = 'Votre espace d\'administration web vous permet d\'ajouter facilement des fichiers à Piwigo, en un seul clic.<br>Vous pouvez aussi synchroniser un répertoire de votre disque dur ou importer des fichiers depuis l\'une des applications compatibles : digiKam, Lightroom, Shotwell…';
 
 $lang['upload sub2 title'] = 'Ajoutez des photos depuis votre téléphone';
