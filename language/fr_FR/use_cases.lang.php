8ea372ce1555a69bc3cc292af7ff9371bae4d6fe
clairem44 <127497258+clairem44@users.noreply.github.com>
2026-08-21 10:31:16 +0200

    Update use_cases.lang.php - Correction claire

--- language/fr_FR/use_cases.lang.php
+++ language/fr_FR/use_cases.lang.php
@@ -1,27 +1,27 @@
 <?php
 
-$lang['scales with your business'] = 'Évolue avec votre activité';
+$lang['scales with your business'] = 'Grandit avec votre activité';
 $lang['use cases header btn'] = 'Démarrer mon essai gratuit';
-$lang['secure collaboration'] = 'Une collaboration sécurisée';
+$lang['secure collaboration'] = 'Collaboration sécurisée';
 $lang['built to last'] = 'Conçu pour durer';
-$lang['easy for everyone to use'] = 'Facile à utiliser pour tout le monde';
-$lang['scales with your library'] = 'Évolue avec votre médiathèque';
-$lang['scales with your organization'] = 'Évolue avec votre organisation';
+$lang['easy for everyone to use'] = 'Ergonomique et facile à prendre en main';
+$lang['scales with your library'] = 'Grandit avec votre photothèque';
+$lang['scales with your organization'] = 'Grandit avec votre organisation';
 $lang['built for collaboration'] = 'Pensé pour la collaboration';
 $lang['your data stays yours'] = 'Vos données restent les vôtres';
 $lang['open source and sustainable'] = 'Open source et durable';
 $lang['use cases tourism challenge title2'] = 'Les difficultés les plus fréquentes :';
-$lang['built for growing photo libraries'] = 'Conçu pour les photothèques qui grandissent';
-$lang['powerful, yet easy to use'] = 'Puissant, et pourtant simple à utiliser';
-$lang['highly customizable'] = 'Hautement personnalisable';
+$lang['built for growing photo libraries'] = 'Conçu pour les photothèques en croissance';
+$lang['powerful, yet easy to use'] = 'Puissant mais facile à utiliser';
+$lang['highly customizable'] = 'Personnalisation poussée';
 $lang['your photos stay yours'] = 'Vos photos restent les vôtres';
 $lang['open source and built to last'] = 'Open source et conçu pour durer';
 
 $lang['use cases public use btn text'] = 'Lire le témoignage client';
 
 $lang['use cases public comments btn'] = 'Découvrir d\'autres témoignages clients';
-$lang['use cases discover title'] = 'Découvrez les fonctionnalités qui se cachent derrière ces cas d\'usage';
-$lang['use cases discover description'] = 'De la recherche avancée aux permissions d\'accès, en passant par la gestion des métadonnées et les galeries publiques, Piwigo fournit tous les outils dont les organisations touristiques ont besoin pour organiser, gérer et partager efficacement leurs contenus visuels.';
+$lang['use cases discover title'] = 'Découvrez les fonctionnalités qui se cachent derrière ces usages';
+$lang['use cases discover description'] = 'De la recherche avancée aux permissions d\'accès, en passant par la gestion des métadonnées et les galeries publiques, Piwigo fournit tous les outils dont les organisations ont besoin pour organiser, gérer et partager efficacement leurs contenus visuels.';
 $lang['use cases discover btn text'] = 'Découvrir les fonctionnalités de Piwigo';
 
 $lang['use cases public host title'] = 'Hébergez Piwigo comme vous le souhaitez';
