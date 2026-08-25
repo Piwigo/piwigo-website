b5b8a72fdc0e9666ca10cca20091b870cb09e972
Martin COZIC <martin.cozic@etu.univ-nantes.fr>
2026-08-25 13:54:40 +0200

    deutsch str cleanup

--- language/de_DE/coding_activity.lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-// +-----------------------------------------------------------------------+
-// | This file is part of Piwigo.                                          |
-// |                                                                       |
-// | For copyright and license information, please view the COPYING.txt    |
-// | file that was distributed with this source code.                      |
-// +-----------------------------------------------------------------------+
-$lang['Coding activity'] = 'Kodierungstätigkeit';
-$lang['porg_codingactivity_intro'] = 'Neueste Commits in Piwigo Core, Plugins, Themes und anderen Tools. Diese Seite sammelt Daten aus verschiedenen Code-Repositories, um einen "One Place to Watch" zu bieten.';
