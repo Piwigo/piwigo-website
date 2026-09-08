5dc8c191b234dc67e733933d83b4ccb1e7597ffc
plegall <plg@piwigo.org>
2026-09-08 10:58:08 +0200

    add custom meta title/desc for the book-a-meeting page

--- language/en_UK/book_a_meeting.lang.php
+++ language/en_UK/book_a_meeting.lang.php
@@ -1,4 +1,6 @@
 <?php 
+$lang['page_meta_title'] = 'Book a meeting with Piwigo';
+$lang['page_meta_description'] = 'Plan your free one-hour meeting with an expert to get a presentation of our services, a demo of the tool, and answers to your questions.';
 
 $lang['book_meeting_title'] = '<span class="orange-text">Book a meeting</span><br>with our staff';
 $lang['book_meeting_desc'] = 'Plan your free one-hour meeting with an expert to get a presentation of our services, a demo of the tool, and answers to your questions.';
--- language/fr_FR/book_a_meeting.lang.php
+++ language/fr_FR/book_a_meeting.lang.php
@@ -1,4 +1,6 @@
 <?php
+$lang['page_meta_title'] = 'Prendre rendez-vous avec Piwigo';
+$lang['page_meta_description'] = 'Planifiez votre rendez-vous gratuit d\'une heure avec un expert pour une présentation, une démo et des réponses.';
 
 $lang['book_meeting_title'] = '<span class="orange-text">Réservez un rendez-vous</span><br>avec notre équipe';
 $lang['book_meeting_desc'] = 'Planifiez votre rendez-vous gratuit d\'une heure avec un expert pour obtenir une présentation de nos services, une démonstration de l\'outil et des réponses à vos questions.';
