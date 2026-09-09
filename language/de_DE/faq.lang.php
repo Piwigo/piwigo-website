972a13c50f0a9de7c558e073cf263c60c02121ac
manuepiwigo <manue@piwigo.com>
2026-09-09 12:11:21 +0200

    German translation fixes - Price page - FAQ

--- language/de_DE/faq.lang.php
+++ language/de_DE/faq.lang.php
@@ -1,29 +1,13 @@
 <?php
 $lang['faq question 1'] = 'Gibt es eine kostenlose Version von Piwigo?';
-$lang['faq answer 1'] = 'Nach Ablauf der Testphase sind alle Tarife von piwigo.com kostenpflichtig. Sie können Piwigo aber kostenlos auf piwigo.org herunterladen und auf dem Server Ihrer Wahl installieren. Dafür brauchen Sie eine vorhandene Infrastruktur sowie die Kenntnisse, um Installation, Updates und Backups von Piwigo sowie Einrichtung und Anpassung selbst zu übernehmen.';
+$lang['faq answer 1'] = 'Nach Ablauf der Testphase sind alle Tarife von Piwigo Cloud kostenpflichtig. Sie können Piwigo aber kostenlos auf piwigo.org herunterladen und auf dem Server Ihrer Wahl installieren. Dafür brauchen Sie eine vorhandene Infrastruktur sowie die Kenntnisse, um Installation, Updates und Backups von Piwigo sowie Einrichtung und Anpassung selbst zu übernehmen.';
 
 $lang['faq question 2'] = 'Fallen bei Piwigo Einrichtungsgebühren an?';
 $lang['faq answer 2'] = 'Bei Piwigo berechnen wir <span class="strong-text">keine</span> Einrichtungsgebühren für die Bereitstellung Ihres Piwigo Hostings.<br><br>So können wir transparente und bezahlbare Preise anbieten - anders als manche Digital-Asset-Management- oder Mediathek-Software, die neben dem Software-Abonnement zahlreiche Einrichtungs- oder Beratungsleistungen in Rechnung stellt.<br><br>Das ist eine bewusste Entscheidung: Unsere Aufgabe ist es, großartige Software bereitzustellen, nicht Beratungsstunden zu verkaufen. Wir bieten weder Projektmanagement noch Beratungsleistungen an. Wenn Sie Hilfe beim Einstieg in Piwigo brauchen, wenden Sie sich einfach an unser Support-Team.<br><br>Wir arbeiten allerdings mit Partnern aus dem Bereich Bildarchiv und Bildredaktion zusammen, die Sie unterstützen können, wenn Sie beim Strukturieren und Einrichten Ihres Piwigo konkrete Begleitung wünschen. <a href="contact">Kontaktieren Sie uns</a>, wenn Sie das interessiert.';
 
 $lang['faq question 3'] = 'Was passiert, wenn mein Speicherplatz nicht mehr ausreicht?';
-$lang['faq answer 3'] = 'Ihr Konto wird nicht automatisch gesperrt. Wir melden uns bei Ihnen und schlagen Ihnen vor, in einen höheren Tarif zu wechseln oder zusätzlichen Speicher zu buchen.<br><br>Der Preis wird zu Ihrem Jahresabonnement hinzugerechnet. Es gelten folgende Sätze (jährliche Abrechnung):<br><br></p>
-                    <ul class="ml-3">
-                      <li>
-                          <p class="size">50 GB : 5 € zzgl. MwSt. / Monat</p>
-                      </li>
-                      <li>
-                          <p class="size">100 GB : 10 € zzgl. MwSt. / Monat</p>
-                      </li>
-                      <li>
-                          <p class="size">250 GB : 25 € zzgl. MwSt. / Monat</p>
-                      </li>
-                      <li>
-                          <p class="size">500 GB : 50 € zzgl. MwSt. / Monat</p>
-                      </li>
-                      <li>
-                          <p class="size">1000 GB : 100 € zzgl. MwSt. / Monat</p>
-                      </li>
-                    </ul><p>';
+$lang['faq answer 3'] = 'Ihr Konto wird nicht automatisch gesperrt. Wir melden uns bei Ihnen und schlagen Ihnen vor, in einen höheren Tarif zu wechseln oder zusätzlichen Speicher zu buchen.<br><br>Der Preis wird zu Ihrem Jahresabonnement hinzugerechnet.</p>
+                    <p>';
 
 $lang['faq question 4'] = 'Können mehrere Piwigo in einem Abonnement enthalten sein?';
 $lang['faq answer 4'] = 'Nein, pro Abonnement ist ein Piwigo vorgesehen.';
@@ -31,7 +15,7 @@ $lang['faq answer 4'] = 'Nein, pro Abonnement ist ein Piwigo vorgesehen.';
 $lang['faq question 5'] = 'Wie lautet die Webadresse meines Piwigo?';
 $lang['faq answer 5'] = 'Standardmäßig lautet die URL Ihres Piwigo nach der Kontoerstellung [ihrbenutzername].piwigo.com. Wir können Piwigo auf Wunsch auch mit Ihrem eigenen Domainnamen verbinden (zum Beispiel media.meinteam.de). Wenden Sie sich dafür einfach an den Support.';
 
-$lang['faq question 6'] = 'Ich habe bereits ein Piwigo auf meinem Server, kann ich es zu Piwigo.com umziehen?';
+$lang['faq question 6'] = 'Ich habe bereits ein Piwigo auf meinem Server, kann ich es zu Piwigo Cloud umziehen?';
 $lang['faq answer 6'] = 'Wenn Sie bereits ein Piwigo auf Ihrer eigenen Infrastruktur betreiben und ein Hosting-Angebot buchen möchten, ist das möglich! Wir übertragen Ihre Daten kostenlos.';
 
 
